a04c7eed476d33979fde91736e79e67afaa8d81e
giuliano monti <giulianm@gmail.com>
2022-03-16 21:44:01 -0300

    feat. adding security tomorrow

---
+++ src/main/java/com/giulian/banco/controller/AuthController.java
@@ -0,0 +1,92 @@
+package com.giulian.banco.controller;
+
+import com.g.springboot.entity.Role;
+import com.g.springboot.entity.User;
+import com.g.springboot.payload.JWTAuthResponse;
+import com.g.springboot.payload.LoginDto;
+import com.g.springboot.payload.SignUpDto;
+import com.g.springboot.repository.RoleRepository;
+import com.g.springboot.repository.UserRepository;
+import com.g.springboot.security.JwtTokenProvider;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@Api(value = "Auth controller exposes siginin and signup REST APIs")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto) {
+
+        // add check for username exist in db
+
+        if (userRepository.existsByUsername(signUpDto.getUsername())) {
+            return new ResponseEntity<>("Username is already Taken!", HttpStatus.OK);
+        }
+
+        // add check for email exists in DB
+
+        if (userRepository.existsByEmail(signUpDto.getEmail())) {
+            return new ResponseEntity<>("Email is already Taken!", HttpStatus.BAD_REQUEST);
+        }
+        // create user object
+
+        User user = new User();
+        user.setName(signUpDto.getName());
+        user.setUsername(signUpDto.getUsername());
+        user.setEmail(signUpDto.getEmail());
+        user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+
+        userRepository.save(user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JWTAuthResponse> authenticateUser(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate
+                (new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        // get token form tokenProvider
+        String token = tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTAuthResponse(token));
+    }
+
+
+
+}
---
+++ src/main/java/com/giulian/banco/payload/LoginDto.java
@@ -0,0 +1,9 @@
+package com.giulian.banco.payload;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String usernameOrEmail;
+    private String password;
+}
