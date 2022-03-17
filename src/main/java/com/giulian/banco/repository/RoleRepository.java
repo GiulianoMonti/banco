3ffdcf5519cbeefff68c8bbc7cebfd3f1d5467ee
giuliano monti <giulianm@gmail.com>
2022-03-16 21:48:04 -0300

    feat sec

---
+++ src/main/java/com/giulian/banco/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.giulian.banco.repository;
+
+import com.g.springboot.entity.Role;
+import com.giulian.banco.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
+
+
