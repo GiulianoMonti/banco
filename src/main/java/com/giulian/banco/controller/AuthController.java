ee89cfdbfe6ac1750cb7a57f82416647a268e183
giuliano monti <giulianm@gmail.com>
2022-03-16 21:48:34 -0300

    feat sec

--- src/main/java/com/giulian/banco/controller/AuthController.java
+++ src/main/java/com/giulian/banco/controller/AuthController.java
@@ -1,13 +1,5 @@
 package com.giulian.banco.controller;
 
-import com.g.springboot.entity.Role;
-import com.g.springboot.entity.User;
-import com.g.springboot.payload.JWTAuthResponse;
-import com.g.springboot.payload.LoginDto;
-import com.g.springboot.payload.SignUpDto;
-import com.g.springboot.repository.RoleRepository;
-import com.g.springboot.repository.UserRepository;
-import com.g.springboot.security.JwtTokenProvider;
 import com.giulian.banco.model.Role;
 import com.giulian.banco.model.User;
 import com.giulian.banco.payload.JWTAuthResponse;
@@ -16,8 +8,6 @@ import com.giulian.banco.payload.SignUpDto;
 import com.giulian.banco.repository.RoleRepository;
 import com.giulian.banco.repository.UserRepository;
 import com.giulian.banco.security.JwtTokenProvider;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
-@Api(value = "Auth controller exposes siginin and signup REST APIs")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
